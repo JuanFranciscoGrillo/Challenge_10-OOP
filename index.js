c75a143bda55aae3f13483394e45d4cfd35dd69c
JuanFranciscoGrillo <134559638+JuanFranciscoGrillo@users.noreply.github.com>
2023-08-31 17:53:58 -0500

    Fixed Jest Test Error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Triangle, Circle, Square } = require('./lib/shapes');
+const { Triangle, Circle, Square } = require('./lib/shapes'); // Corrected relative path
 
 async function getUserInput() {
   const userInput = await inquirer.prompt([
@@ -26,12 +26,31 @@ async function getUserInput() {
       name: 'shapeColor',
       message: 'Enter the shape color (color keyword or hex):',
     },
+    // New prompts for SVG dimensions
+    {
+      type: 'input',
+      name: 'svgWidth',
+      message: 'Enter the width of the SVG (in pixels):',
+      default: '300',
+    },
+    {
+      type: 'input',
+      name: 'svgHeight',
+      message: 'Enter the height of the SVG (in pixels):',
+      default: '300',
+    },
   ]);
 
   const shape = getUserSelectedShape(userInput.shape);
   shape.setColor(userInput.shapeColor);
 
-  const svg = generateSVG(userInput.text, userInput.textColor, shape.render());
+  const svg = generateSVG(
+    userInput.text,
+    userInput.textColor,
+    shape.render(),
+    userInput.svgWidth,
+    userInput.svgHeight
+  );
   saveSVGToFile(svg);
 }
 
@@ -46,13 +65,12 @@ function getUserSelectedShape(shapeName) {
   }
 }
 
-function generateSVG(text, textColor, shapeContent) {
-  return `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-    <style>
-      text { fill: ${textColor}; font-family: Arial, sans-serif; font-size: 24px; }
-    </style>
-    <text x="50" y="50">${text}</text>
+function generateSVG(text, textColor, shapeContent, svgWidth, svgHeight) {
+  const fontSize = 48; // Adjust the font size as needed
+
+  return `<svg width="${svgWidth}" height="${svgHeight}" xmlns="http://www.w3.org/2000/svg">
     ${shapeContent}
+    <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" fill="${textColor}" font-family="Arial, sans-serif" font-size="${fontSize}">${text}</text>
   </svg>`;
 }
 
